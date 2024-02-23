8246c9b97dcab5019ef1f12f54cd2a4f60169beb
KBR <kbr@ucl.dk>
2024-02-23 10:11:56 +0100

    Initial chapter06

---
+++ Dapr/chapter06/sample.microservice.reservation/State/ReservationState.cs
@@ -0,0 +1,9 @@
+namespace sample.microservice.state.reservation;
+
+public class ReservationState
+{
+    public Guid OrderId { get; set; }
+
+    public List<ItemReservation>? ReservedItems { get; set; }
+}
+    
