aef52c78b9c4b23d8024aa5b86004ca05f4bb6a1
KBR <kbr@ucl.dk>
2024-02-29 17:04:06 +0100

    Launch.ps1 - RabbitMq added

--- Dapr/chapter08/launch.ps1
+++ Dapr/chapter08/launch.ps1
@@ -1,3 +1,6 @@
+docker run -d -p 5672:5672 -p 15672:15672 --name dtc-rabbitmq rabbitmq:3-management
+http://localhost:15672/
+
 dapr run --app-id "order-service" --app-port "5001" --dapr-grpc-port "50010" --dapr-http-port "5010" --components-path "./components" -- dotnet run --project ./sample.microservice.order/sample.microservice.order.csproj --urls="http://+:5001"
 dapr run --app-id "reservation-service" --app-port "5002" --dapr-grpc-port "50020" --dapr-http-port "5020" --components-path "./components" -- dotnet run --project ./sample.microservice.reservation/sample.microservice.reservation.csproj --urls="http://+:5002"
 dapr run --app-id "reservationactor-service" --app-port "5004" --dapr-grpc-port "50040" --dapr-http-port "5014" --components-path "./components" -- dotnet run --project ./sample.microservice.reservationactor.service/sample.microservice.reservationactor.service.csproj --urls="http://+:5004"
